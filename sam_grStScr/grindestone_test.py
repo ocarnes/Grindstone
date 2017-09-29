3c1ac52a909ae0eea48322763e1df213ae66ad20
sadams115 <sadams115@gmail.com>
2017-09-29 14:37:52 -0600

    Add files via upload
    Grindstone window test

---
+++ sam_grStScr/grindestone_test.py
@@ -0,0 +1,103 @@
+import sys
+import maya.api.OpenMaya as OpenMaya
+import maya.cmds as cmds
+
+#sys.path.append('C:\Users\adamsse\Documents\maya\2018\scripts')
+#reload(grindestone_test)
+#import grindestone_test as gs
+
+
+
+
+class GrindestoneWindow:
+    
+    winID = "grindstoneWindow"
+    
+    
+    
+    def __init__(self):
+        pass
+        
+        
+        
+    def printTxtField(self, fieldID):
+        print cmds.textField(fieldID, query=True, text=True)
+        
+        
+        
+    def run(self):
+        # Test to make sure that the UI isn't already active
+        if cmds.window(self.winID, exists=True):
+            cmds.deleteUI(self.winID)
+    
+        # Now create a fresh UI window
+        cmds.window(self.winID)
+
+        # Add a Layout - a columnLayout stacks controls vertically
+        cmds.columnLayout()
+
+        # Add controls into this Layout
+        message = cmds.textField()
+        cmds.button(label='click me', command=lambda *args: self.printTxtField( message ))
+        #cmds.button(label='click me', command='print "Hello!"')
+        #cmds.button(label='click me', command='print cmds.textField(self.message, query=True, text=True)')
+
+        # Display the window
+        cmds.showWindow()
+
+
+
+
+##########################################################
+# Plug-in 
+##########################################################
+class MyCommandClass( OpenMaya.MPxCommand ):
+    kPluginCmdName = 'mimic'
+    
+    
+    windowProvider = GrindestoneWindow()
+    
+    
+    def __init__(self):
+        ''' Constructor. '''
+        OpenMaya.MPxCommand.__init__(self)
+    
+    @staticmethod 
+    def cmdCreator():
+        ''' Create an instance of our command. '''
+        return MyCommandClass() 
+        
+    
+    def doIt(self, args):
+
+       self.windowProvider.run()
+    
+    
+##########################################################
+# Plug-in initialization.
+##########################################################
+
+def maya_useNewAPI():
+	"""
+	The presence of this function tells Maya that the plugin produces, and
+	expects to be passed, objects created using the Maya Python API 2.0.
+	"""
+	pass
+
+def initializePlugin( mobject ):
+    ''' Initialize the plug-in when Maya loads it. '''
+    mplugin = OpenMaya.MFnPlugin( mobject )
+    try:
+        mplugin.registerCommand( MyCommandClass.kPluginCmdName, 
+            MyCommandClass.cmdCreator )
+
+    except:
+        sys.stderr.write( 'Failed to register command: ' + MyCommandClass.kPluginCmdName )
+
+def uninitializePlugin( mobject ):
+    ''' Uninitialize the plug-in when Maya un-loads it. '''
+    mplugin = OpenMaya.MFnPlugin( mobject )
+    try:
+        mplugin.deregisterCommand( MyCommandClass.kPluginCmdName )
+    except:
+        sys.stderr.write( 'Failed to unregister command: ' + MyCommandClass.kPluginCmdName )

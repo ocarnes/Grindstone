013aa1ce96649fad20ba685795e31e24d87ed7e1
Rusyuu <samuel.carnes@colorado.edu>
2017-10-04 15:00:05 -0600

    Functional, will implement in plugin

--- sam_grStScr/byeButton.py
+++ sam_grStScr/byeButton.py
@@ -1,11 +1,11 @@
-import pymel.core as pm
-shelfButtons=pm.shelfLayout("Custom", q=1, childArray=1)
+import maya.cmds
+shelfButtons=cmds.shelfLayout("Custom", q=1, ca=1)
 for button in shelfButtons:
-	label = ""
-	# Assert that this is a shelfButton 
-	if pm.objectTypeUI(button, isType="shelfButton"):
-		label=str(pm.shelfButton(button, q=1, label=1))
-		# If this button has the label we're looking for, 
-		# delete the button. 
-		if "helloButton" == label:
-			pm.deleteUI(button)
+    label = ""
+    # Assert that this is a shelfButton 
+    if cmds.objectTypeUI(button, isType="shelfButton"):
+        label=str(cmds.shelfButton(button, q=1, label=1))
+        # If this button has the label we're looking for, 
+        # delete the button. 
+        if "helloButton" == label:
+            cmds.deleteUI(button)

c567d9b984267de268d951257b941bac3ff6f099
Sean Adams <sadams115@gmail.com>
2017-11-03 16:11:59 -0600

    Added FileTypeMA check and tweaked naming conventions.

---
+++ plug-ins/gs_assets/gs_scripts/Common/gs_FileTypeMA_class.py
@@ -0,0 +1,31 @@
+# Grindstone gs_FileTypeMA_class.py
+# Authors: Sam Carnes and Sean Adams
+
+# This file defines a script that checks to see if the file type is .ma
+
+
+import maya.cmds as cmds
+
+class FileTypeMA:
+    
+    
+    def __init__(self):
+        pass
+       
+        
+    def doCheck(self):
+        
+        # get a string that contains the file type
+        fileType = cmds.file(query=True, type=True)
+        
+        if "mayaAscii" in fileType:
+            return ''
+            
+        else:
+            returnString = "File type is \"" + fileType[0] + ",\" and should be \"mayaAscii.\""
+            
+            return returnString
+        
+        
+def getObject():
+    return FileTypeMA()
